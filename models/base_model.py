288c1041c916850a4dc2204a7cb766ed347f0065
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-10-15 18:38:53 +0100

    changed setattr and env

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 base_model module: contains a class `BaseModel` that defines all common
 attributes/methods for other classes
@@ -47,7 +47,7 @@ class BaseModel:
                     continue
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                self.__dict__[key] = value
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
