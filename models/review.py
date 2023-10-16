5933cc657d478746c23bb1fde31eb35a6bd62bda
ihuomalyndha <ihuomalyndha@gmail.com>
2023-10-16 01:09:54 +0100

    review model update

--- models/review.py
+++ models/review.py
@@ -6,6 +6,10 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """This file defines the Review Model
+It inherits from the BaseModel
+"""
+
     place_id = ""
     user_id = ""
     text = ""
